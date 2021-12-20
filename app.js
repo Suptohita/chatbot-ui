614e56ee6df5b58d7cef524f9220bfe781fe6d8f
Arvind <arbindudebnath59985@gmail.com>
2021-12-20 22:47:16 +0600

    complete chatbot

---
+++ app.js
@@ -0,0 +1,9 @@
+function toggleFunction () {
+    const toggle = document.getElementById('extended_chat')
+    if(toggle.style.display=='block'){
+        toggle.style.display = 'none'
+    }
+    else{
+        toggle.style.display='block'
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+I used tailwind and animate.css library
+It is responsive but it look soo cool in wider device.
